dd80c9ace37ca4119d49e129e02fd6e4291fbbb7
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-03-21 10:20:31 +0100

    Created SignIn screen

--- app/src/main/java/com/example/ibustartup/ui/theme/Color.kt
+++ app/src/main/java/com/example/ibustartup/ui/theme/Color.kt
@@ -2,6 +2,12 @@ package com.example.ibustartup.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val DarkBlue = Color(0xFF00396C)
+val LightBlue  = Color(0xFF008DDA)
+val GrayBackground = Color(0xFF656565)
+val GrayStroke = Color(0xFFD9D9D9)
+val Yellow = Color(0xFFE8E87C)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
