c71786e7aa5a4a602da9ac50e13b90a3c9775f05
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-05-26 09:06:27 +0200

    Slight Bugfix

--- app/src/main/java/com/example/ibustartup/ui/components/Navigation.kt
+++ app/src/main/java/com/example/ibustartup/ui/components/Navigation.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.ibuPosition.backend.viewmodels.PositionViewModel
+import com.example.ibustartup.backend.viewmodels.PositionViewModel
 import com.example.ibustartup.R
 import com.example.ibustartup.backend.tables.Startup
 import com.example.ibustartup.backend.viewmodels.StartupEvent
